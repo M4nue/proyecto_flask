dcd6f0d84b9f45e2cd391041aa5fe1cae3d10495
M4nue <mrodcur12042005@gmail.com>
2024-04-23 08:15:56 +0200

    modificación ruta /lista

--- app.py
+++ app.py
@@ -13,14 +13,19 @@ def buscador():
 
 @app.route('/lista', methods=["post"])
 def lista():
-    nombre=request.form.get("Caja")
-    
-    if nombre == archivo_json["nombre"]:
-        return render_template("lista.html", nombre=archivo_json["nombre"], precio=archivo_json["precio_compra"], imagen=archivo_json["imagen"])
-    elif nombre == "":
-        for i in archivo_json:
-           return render_template("lista.html", nombre=i["nombre"], precio=i["precio_compra"], imagen=i["imagen"])
-    else:
-        return print('La caja',nombre,'no existe') 
+    nombre = request.form.get("Caja")
+    coincidencias_nombres_caja = []
+    precio_compra = []
+    precio_venta = []
+
+    for caja in archivo_json:
+        if nombre == caja["nombre"]:
+            return render_template("lista.html", nombre=caja["nombre"], precio_compra=caja["precio_compra"], precio_venta=caja["sale_price_text"])
+        else:
+            coincidencias_nombres_caja.append(caja["nombre"])
+            precio_compra.append(caja["precio_compra"])
+            precio_venta.append(caja["sale_price_text"])
+    return render_template("lista.html", cajas=coincidencias_nombres_caja, precio_compra=precio_compra,precio_venta=precio_venta) 
+
 
 app.run("0.0.0.0",5000,debug=True)
